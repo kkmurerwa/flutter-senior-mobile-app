6a0a813d3ad7231c6cd114d18b0ac5bfb53f4ea7
Kenneth Murerwa <xwaxes@gmail.com>
2023-01-30 16:09:33 +0300

    feat: Modified orders repository and its implementation to reflect new changes

---
+++ test/features/orders/data/repositories/orders_repository_impl_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+
+class MockOrders
+class MockNetworkInfo extends Mock implements NetworkInfo {}
+
+void main() {
+
+
+
+  test('has internet check should return correct internet state', () {
+    // arrange
+
+    // act
+
+    // assert
+  });
+}
