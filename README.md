67483ebf4c778f7115c7172fd7ae4b8368040a85
Kenneth Murerwa <xwaxes@gmail.com>
2023-01-30 14:23:13 +0300

    feat: Initial commit with default Flutter setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# flutter_senior_mobile_app
+
+A Flutter project for the Amitruck senior mobile developer role
+
+## Getting Started
+
+
---
+++ android/app/src/main/kotlin/com/murerwa/flutter_senior_mobile_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.murerwa.flutter_senior_mobile_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
